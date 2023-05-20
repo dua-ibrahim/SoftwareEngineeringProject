abd5cc9174f6673511f75c2e1b4bc3435cd21328
lenovo <lenovo@DESKTOP-F74HTM3>
2023-05-20 08:08:08 -0700

    First Commit

---
+++ Myworkecorner/src/myproject/Myproject.java
@@ -0,0 +1,12 @@
+package myproject;
+
+public class Myproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mywork MyworkObject = new Mywork ();
+		MyworkObject.computerLab ();
+
+	}
+
+}
---
+++ Myworkecorner/src/myproject/Mywork.java
@@ -0,0 +1,12 @@
+package myproject;
+
+public class Mywork {
+
+	public void computerLab() {
+		System.out.println(" hello world");
+		
+			
+
+	}
+
+}
