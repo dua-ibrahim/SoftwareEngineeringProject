c085236de5997093d93d97b6d461afd0301e21d4
5nober <134306768+5nober@users.noreply.github.com>
2023-05-22 20:49:37 -0700

    Update Mywork.java

--- Myworkecorner/src/myproject/Mywork.java
+++ Myworkecorner/src/myproject/Mywork.java
@@ -4,7 +4,8 @@ public class Mywork {
 
 	public void computerLab() {
 		System.out.println(" hello world");
-		
+		System.out.println("Project Partners:Dua Ibrahim[2K22/CSM/30] and Sanobar Ali[2K22/CSM/104]")
+		System.out.println(" Those who can imagine anything,can create the impossible")	
 			
 
 	}
